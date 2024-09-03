7fb53754f0dc10c3e80e76e40852b5254ab92320
svds90 <svds22090@gmail.com>
2024-09-03 23:30:31 +0300

    upd

--- link_collector/src/collector.py
+++ link_collector/src/collector.py
@@ -1,24 +1,42 @@
+from os import link
+from urllib import request
 from utils import CollectorConfig, ContentLink
 from fake_useragent import UserAgent
 
 import logging
 import requests
+import lxml
 from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(levelname)s:%(message)s')
 
 
-class CollectorInit:
+class LinkCollector:
     def __init__(self) -> None:
         self.config = CollectorConfig()
-        self.link = ContentLink(self.config.primary_domain, self.config.content_type)
+        self.url = ContentLink(self.config.primary_domain, self.config.content_type)
         self.user_agent = UserAgent().random
         self.headers = {'user-agent': self.user_agent}
 
+    def get_page_count(self):
+        request = requests.get(str(self.url), headers=self.headers)
+        soup = BeautifulSoup(request.text, 'lxml')
+        navigation_block = soup.find('div', class_='b-navigation')
+        last_page = navigation_block.find_all('a', href=True,
+                                              string=lambda text: text and text.isdigit())[-1]
+        return last_page.text
 
-collector = CollectorInit()
 
-print(collector.headers)
+link_collector = LinkCollector()
+print(link_collector.get_page_count())
+
+# soup = BeautifulSoup(html, 'lxml')
+# nav_block = soup.find('div', class_='b-navigation')
+# last_a = nav_block.find_all('a')[-1]
+# print(last_a.text)  # Выведет "1390"
+
+# request1 = requests.get(str(link_collector.url), headers=link_collector.headers)
+# print(request1.text)
 
 #
 # if soup:
