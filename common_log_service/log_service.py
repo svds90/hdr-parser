5af9babfb034437f973c99a3bd4406a49446eb93
svds90 <svds22090@gmail.com>
2024-10-07 20:37:29 +0300

    upd

--- common_log_service/log_service.py
+++ common_log_service/log_service.py
@@ -1,5 +1,5 @@
 import logging
-import os
+import requests
 
 
 class CommonLogger:
@@ -8,32 +8,65 @@ class CommonLogger:
 
     def __init__(self, logger_config: dict):
         self.logger_name = logger_config["logger_name"]
-        self.log_level = logger_config["log_level"].upper()
+        self.common_log_level = logger_config["log_level"]["common"].upper()
+        self.file_handler_log_level = logger_config["log_level"]["file_handler"].upper()
+        self.console_handler_log_level = logger_config["log_level"]["console_handler"].upper()
+        self.log_dir = logger_config["log_dir"]
         self.log_filename = logger_config["log_filename"]
         self.log_filemode = logger_config["log_filemode"]
+        self.logger = self.setup_logger()
 
-        self.logger = logging.getLogger(self.logger_name)
-        self.logger.setLevel(getattr(logging, self.log_level))
+    def setup_logger(self):
+        """Set up the main logger and its handlers."""
 
-        file_handler = logging.FileHandler(self.log_filename, self.log_filemode)
+        logger = logging.getLogger(self.logger_name)
+        logger.setLevel(getattr(logging, self.common_log_level))
+
+        self.setup_file_handler(logger)
+        self.setup_console_handler(logger)
+
+        return logger
+
+    def setup_file_handler(self, logger):
+        """Configure the file handler for logging to a file."""
+
+        file_handler = logging.FileHandler(f"{self.log_dir}/{self.log_filename}", self.log_filemode)
+        file_handler.setLevel(getattr(logging, self.file_handler_log_level))
         formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)-7s - %(message)s - [%(filename)s]')
         file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
-        self.logger.addHandler(file_handler)
+    def setup_console_handler(self, logger):
+        """Configure the console handler for logging to the console."""
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(getattr(logging, self.console_handler_log_level))
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)-7s - %(message)s - [%(filename)s]')
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
 
 
-settings = {"logger_name": "link_collecor", "log_level": "info",
-            "log_filename": "test.log", "log_filemode": "a"}
+settings = {
+    "logger_name": "link_collector",
+    "log_dir": "/var/log/hdr_parser",
+    "log_filename": "link_collector.log",
+    "log_filemode": "a",
+    "log_level": {
+        "common": "info",
+        "file_handler": "info",
+        "console_handler": "info"
+    }
+}
+
+r = requests.get("https://www.google.com")
+
 mylogger = CommonLogger(settings)
 
 mylogger.logger.info('some info')
 
 try:
-    10 / 0
+    a = 10 / 0
 except Exception as e:
     mylogger.logger.error('%s', e, exc_info=e)
-
-project_root = os.path.basename(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-print(project_root)
